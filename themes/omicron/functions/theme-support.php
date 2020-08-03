17fea311e2004d6b49eafe9a2cccfbc381f0e5ec
RebeccaBrewster <r.brewster@firm-media.com>
2020-08-02 23:15:01 -0700

    Added Health Rating column

--- themes/omicron/functions/theme-support.php
+++ themes/omicron/functions/theme-support.php
@@ -52,21 +52,27 @@ function joints_theme_support() {
 	
 } /* end theme support */
 
-add_action('acf/init', 'my_acf_init');
-function my_acf_init() {
+
+if( function_exists('acf_add_options_page') ) {
 	
-	// check function exists
-	if( function_exists('acf_register_block') ) {
-		
-		// register a testimonial block
-		acf_register_block(array(
-			'name'				=> 'testimonial',
-			'title'				=> __('Testimonial'),
-			'description'		=> __('A custom testimonial block.'),
-			'render_template'	=> '/parts/blocks/content-testimonial.php',
-			'category'			=> 'formatting',
-			'icon'				=> 'admin-comments',
-			'keywords'			=> array( 'testimonial', 'quote' ),
-		));
-	}
+	acf_add_options_page(array(
+		'page_title' 	=> 'Adjudicator Dashboard',
+		'menu_title'	=> 'Adjudicator Dashboard',
+		'menu_slug' 	=> 'adjudicator-dashboard',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Theme Header Settings',
+	// 	'menu_title'	=> 'Header',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+	
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Theme Footer Settings',
+	// 	'menu_title'	=> 'Footer',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+	
 }
--- themes/omicron/parts/blocks/content-table.php
+++ themes/omicron/parts/blocks/content-table.php
@@ -1,6 +1,6 @@
 <div class="table-container">
 <?php
-    $table = get_field( 'game_table' );
+    $table = get_field( 'game_table', 'options' );
 
     if ( ! empty ( $table ) ) {
     
@@ -14,14 +14,55 @@
             if ( ! empty( $table['header'] ) ) {
     
                 echo '<thead>';
+
+                $thead_array = array();
     
                     echo '<tr>';
     
                         foreach ( $table['header'] as $th ) {
-    
-                            echo '<th onclick="sortTable(0)">';
-                                echo $th['c'];
-                            echo '</th>';
+                            
+                            array_push($thead_array, $th);
+
+                            if(is_page_template('template-game-table.php')):
+                            
+                                if($th['c'] == 'HP Current'):
+
+                                    echo '';
+
+                                elseif($th['c'] == 'HP Max'):
+                                    
+                                    echo '<th onclick="sortTable(0)">';
+                                        echo 'Health Rating';
+                                    echo '</th>';
+
+                                else:
+
+                                    echo '<th onclick="sortTable(0)">';
+                                        echo $th['c'];
+                                    echo '</th>';
+
+                                endif;
+
+                            else:
+
+                                if($th['c'] == 'HP Max'):
+                                
+                                    echo '<th onclick="sortTable(0)">';
+                                        echo $th['c'];
+                                    echo '</th>';
+                                    
+                                    echo '<th onclick="sortTable(0)">';
+                                        echo 'Health Rating';
+                                    echo '</th>';
+
+                                else:
+
+                                    echo '<th onclick="sortTable(0)">';
+                                        echo $th['c'];
+                                    echo '</th>';
+
+                                endif;
+                            endif;
                         }
     
                     echo '</tr>';
@@ -30,19 +71,190 @@
             }
     
             echo '<tbody>';
+
+                if('faction_post' == get_post_type()):
+                    $faction = basename((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+                endif;
     
                 foreach ( $table['body'] as $tr ) {
-    
-                    echo '<tr>';
-    
-                        foreach ( $tr as $td ) {
-    
-                            echo '<td>';
-                                echo $td['c'];
-                            echo '</td>';
-                        }
-    
-                    echo '</tr>';
+                    
+                    if('faction_post' == get_post_type()):
+
+                        if(strtolower($tr[0]['c']) == $faction):
+
+                            echo '<tr faction=' . $tr[0]['c'] . '>';
+
+                                $table_ctr = 0;
+
+                                $current_health = '';
+            
+                                foreach ( $tr as $td ) {
+
+                                    $column =  preg_replace("/[\s_]/", "-", $thead_array[$table_ctr]['c']);
+
+                                    if($column == 'HP-Max'):
+
+                                        echo '<td column=' . $column .'>';
+                                            echo $td['c'];
+                                        echo '</td>';
+
+                                        echo '<td column=Health-Rating>';
+                                            $health_calc = $current_health / $td['c'];
+                                            $health_rating = '';
+
+                                            switch($health_calc) {
+                                                case $health_calc <= 0.25:
+                                                    $health_rating = 'Dying';
+                                                    break;
+                                                case $health_calc > 0.25 && $health_calc <= 0.75:
+                                                    $health_rating = 'Damaged';
+                                                    break;
+                                                case $health_calc > 0.75 && $health_calc <= 1;
+                                                    $health_rating = 'Healthy';
+                                                    break;
+                                                default:
+                                                    $health_rating = 'Healthy';
+                                            }
+                                                
+                                            echo $health_rating;
+                                        echo '</td>';
+
+                                    elseif($column == 'HP-Current'):
+
+                                        $current_health = $td['c'];
+
+                                        echo '<td column=' . $column .'>';
+                                            echo $td['c'];
+                                        echo '</td>';
+
+                                    else:
+
+                                        echo '<td column=' . $column .'>';
+                                            echo $td['c'];
+                                        echo '</td>';
+
+                                    endif;
+
+                                    $table_ctr ++;
+                                }
+            
+                            echo '</tr>';
+
+                        endif;
+                    
+                    elseif( is_page_template('template-game-table.php')):
+
+                        echo '<tr faction=' . $tr[0]['c'] . '>';
+
+                            $table_ctr = 0;
+
+                            $current_health = '';
+        
+                            foreach ( $tr as $td ) {
+
+                                $column =  preg_replace("/[\s_]/", "-", $thead_array[$table_ctr]['c']);
+
+                                if($column == 'HP-Max'):
+
+                                    echo '<td column=Health-Rating>';
+                                        $health_calc = $current_health / $td['c'];
+                                        $health_rating = '';
+
+                                        switch($health_calc) {
+                                            case $health_calc <= 0.25:
+                                                $health_rating = 'Dying';
+                                                break;
+                                            case $health_calc > 0.25 && $health_calc <= 0.75:
+                                                $health_rating = 'Damaged';
+                                                break;
+                                            case $health_calc > 0.75 && $health_calc <= 1;
+                                                $health_rating = 'Healthy';
+                                                break;
+                                            default:
+                                                $health_rating = 'Healthy';
+                                        }
+                                            
+                                        echo $health_rating;
+                                    echo '</td>';
+
+                                elseif($column == 'HP-Current'):
+
+                                    $current_health = $td['c'];
+
+                                else:
+
+                                    echo '<td column=' . $column .'>';
+                                        echo $td['c'];
+                                    echo '</td>';
+
+                                endif;
+
+                                $table_ctr ++;
+                            }
+        
+                        echo '</tr>';
+
+                    else:
+
+                        echo '<tr faction=' . $tr[0]['c'] . '>';
+
+                            $table_ctr = 0;
+
+                            $current_health = '';
+        
+                            foreach ( $tr as $td ) {
+
+                                $column =  preg_replace("/[\s_]/", "-", $thead_array[$table_ctr]['c']);
+
+                                if($column == 'HP-Max'):
+
+                                    echo '<td column=' . $column .'>';
+                                        echo $td['c'];
+                                    echo '</td>';
+
+                                    echo '<td column=Health-Rating>';
+                                        $health_calc = $current_health / $td['c'];
+                                        $health_rating = '';
+
+                                        switch($health_calc) {
+                                            case $health_calc <= 0.25:
+                                                $health_rating = 'Dying';
+                                                break;
+                                            case $health_calc > 0.25 && $health_calc <= 0.75:
+                                                $health_rating = 'Damaged';
+                                                break;
+                                            case $health_calc > 0.75 && $health_calc <= 1;
+                                                $health_rating = 'Healthy';
+                                                break;
+                                            default:
+                                                $health_rating = 'Healthy';
+                                        }
+                                            
+                                        echo $health_rating;
+                                    echo '</td>';
+
+                                elseif($column == 'HP-Current'):
+
+                                    $current_health = $td['c'];
+
+                                    echo '<td column=' . $column .'>';
+                                        echo $td['c'];
+                                    echo '</td>';
+
+                                else:
+
+                                    echo '<td column=' . $column .'>';
+                                        echo $td['c'];
+                                    echo '</td>';
+
+                                endif;
+
+                                $table_ctr ++;
+                            }
+        
+                        echo '</tr>';
+
+                    endif;
                 }
     
             echo '</tbody>';
